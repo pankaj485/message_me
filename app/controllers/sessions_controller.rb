4677e6ff99738d05c48a1438801bb1934452e2af
pankaj485 <pankajsamar2@gmail.com>
2022-07-07 15:29:27 +0530

    update login page UI, add form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,4 @@
 class SessionsController < ApplicationController
-  def index
-    greeting = "Hello from the"
-    endpoint = "session"
-    route = "It is login route"
-    param = params.to_s
-
-    @message = "#{greeting} #{endpoint} #{route}. param: #{param}"
+  def new
   end
 end
