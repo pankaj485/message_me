95bff7e410d328465cd475e463c87472e72bb769
pankaj485 <pankajsamar2@gmail.com>
2022-07-08 10:48:44 +0530

    add authentication system. create, destroy sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,23 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = "Logged in successfully"
+      redirect_to root_path
+    else
+      # rendering new form again
+      flash.now[:error] = "Can't login, credentials not matching"
+      render "new"
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "Logged out successfully"
+    redirect_to login_path
+  end
 end
