aa89da8812c6033fc0c746af100883812ccb26eb
pankaj485 <pankajsamar2@gmail.com>
2022-07-08 16:40:52 +0530

    limit messages using scopes

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,6 @@
 class Message < ApplicationRecord
   validates :body, presence: true
   belongs_to :user
+  # show last 20 messages only
+  scope :custom_dispaly, -> { order(:created_at).last(20) }
 end
